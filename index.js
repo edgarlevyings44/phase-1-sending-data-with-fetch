236fbb69014ff269da7691adc354513522e0e609
Levy <edgarlevy81@gmail.com>
2023-08-09 01:13:16 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Add your code here
+function submitData(userName, email){
+    const apiUrl = 'http://localhost:3000/users'
+    const postInfo = {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json",
+           
+
+        },
+        body: JSON.stringify({
+            name: userName,
+            email: email
+        })
+
+    }
+    return fetch(apiUrl, postInfo)
+    .then(response => response.json())
+    .then(data => document.body.innerHTML = data.id)
+    .catch(error => {document.body.innerHTML = error.message})
+    }
+    submitData('Steve', 'steve@steve.com')
+    
